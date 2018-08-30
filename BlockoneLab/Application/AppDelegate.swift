0151eda36cdc34e1b270d71e4e68575be6ad5e3c
aarthur <tricpony@mac.com>
2018-08-30 04:57:24 -0400

    good place to lock this down -- services working pretty smoothly

---
+++ BlockoneLab/Application/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  BlockoneLab
+//
+//  Created by aarthur on 8/29/18.
+//  Copyright © 2018 Gigabit LLC. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let sb: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let rootVC = sb.instantiateInitialViewController()
+        
+        self.window?.rootViewController = rootVC
+        
+        return true
+    }
+    
+    // MARK: - Bundle Info
+    
+    func appDisplayName() -> String {
+        let infoDictionary = Bundle.main.infoDictionary
+        return infoDictionary!["CFBundleDisplayName"] as! String
+    }
+    
+    func appID() -> String {
+        let infoDictionary = Bundle.main.infoDictionary
+        return infoDictionary!["CFBundleIdentifier"] as! String
+    }
+    
+}
+
---
+++ BlockoneLab/Application/BlockoneLab-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  BlockoneLab-Bridging-Header.pch
+//  BlockoneLab
+//
+//  Created by aarthur on 8/29/18.
+//  Copyright © 2018 Gigabit LLC. All rights reserved.
+//
+
+#ifndef BlockoneLab_Bridging_Header_pch
+#define BlockoneLab_Bridging_Header_pch
+
+#import <MagicalRecord/MagicalRecord.h>
+
+#endif /* BlockoneLab_Bridging_Header_pch */
